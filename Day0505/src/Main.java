fe6803bec863a51924c8ccd14f19d872cbd64cbf
zhaoyanjie <2512670071@qq.com>
2017-05-05 13:43:58 +0800

    continue commit.

--- Day0505/src/Main.java
+++ Day0505/src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+
 /**
  * This is a test.
  * Created by BFD-592 on 2017/5/5.
  */
 public class Main {
     public static void main(String[] args) {
+        int[] arr = new int[5];
+        Arrays.fill(arr, 1);
         System.out.println("Hello World!");
     }
 }
