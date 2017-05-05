dd52a29a750cfa710bb2ee191edeeadbd8d09399
zhaoyanjie <2512670071@qq.com>
2017-05-05 11:28:32 +0800

    This is a test.

---
+++ Day0505/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * This is a test.
+ * Created by BFD-592 on 2017/5/5.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
